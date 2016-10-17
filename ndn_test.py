09f830756d4b86a31a5dc178f181c8802a7476ab
Peter Duggins <psipeter@gmail.com>
2016-10-17 15:35:21 -0400

    files on workstation before cleanup

--- ndn_test.py
+++ ndn_test.py
@@ -4,7 +4,101 @@ Fall 2016
 Test nengo_detailed_neurons with communication channel and integrator
 '''
 
-def make_df(sim,P,stim_probe,B_LIF_probe,B_NRN_probe,C_LIF_probe,C_NRN_probe):
+# if solver_type == 'whitenoise': rc.set("decoder_cache", "enabled", "False") #don't try to remember old decoders
+# else: rc.set("decoder_cache", "enabled", "True")
+
+def calculate_NRN_decoders(P,model,sim):
+    import neuron
+    import nengo
+    from nengo.processes import WhiteSignal
+    import nengo_detailed_neurons
+    from nengo_detailed_neurons.util import nrn_duration
+    import numpy as np
+
+    class MySolver(nengo.solvers.Solver):
+        #When the simulator builds the network, it looks for a solver to calculate the decoders
+        #instead of the normal least-squares solver, we define our own, so that we can return
+        #decoders calculated using the white noise method
+        def __init__(self,conn): #feed in old decoders
+            self.weights=False #they are not weights but decoders
+            self.my_weights=conn.weights
+        def __call__(self,A,Y,rng=None,E=None): #the function that gets called by the builder
+            decoders=WhiteNoiseDecoders(conn,P)
+            self.my_weights=decoders
+            print self.my_weights
+            return self.my_weights.T, dict()
+
+    #recalculate decoders for every connection into an NRN ensemble
+    for conn in model.all_connections:
+        print isinstance(conn.post_obj,(Ensemble))
+        if type(conn.post) == '<class "nengo.ensemble.Ensemble">' and conn.post.neuron_type=='Bahr2':
+            conn.solver = MySolver(conn,P)
+
+    sim=nengo.Simulator(model,dt=P['dt'])
+    return sim
+
+
+def WhiteNoiseDecoders(connection,P):
+        import neuron
+        import nengo
+        from nengo.processes import WhiteSignal
+        import nengo_detailed_neurons
+        from nengo_detailed_neurons.util import nrn_duration
+        import numpy as np
+
+        ensemble=connection.post
+        function=connection.function
+        transform=connection.transform
+        evalpoints=P['evalpoints']
+        dt=P['dt']
+        tau_filter=P['tau_filter']
+        runtime_per_eval=P['runtime_per_eval']
+
+        #get a white noise signal array
+        t_run=evalpoints*dt
+        with nengo.Network() as model:
+            signal=nengo.Node(output=WhiteSignal(t_run,high=100,rms=1))
+            probe=nengo.Probe(signal)
+        sim=nengo.Simulator(model,dt=dt)
+        sim.run(t_run)
+        sim.close()
+        whitenoise=sim.data[probe]
+
+        #feed this stimulus into each Bahr2 neuron (), propagate dVdt to soma with NEURON,
+        #record spikes, calculate rate by convolution of spikes with synaptic filter, 
+        A=[]
+        for cell in nengo_detailed_neurons.builder.ens_to_cells[ensemble]:
+            apcount = neuron.h.APCount(cell.neuron.soma(0.5)) #set up recording of action potentials
+            iclamp=neuron.h.IClamp(cell.neuron.soma(0.5)) #start by connecting directly to soma
+            neuron.init()
+            spikes=np.zeros_like(whitenoise).ravel()
+            for x in range(len(whitenoise)):
+                #each point in time is an evaluation point, used as a constant iclamp for duration runtime_per_eval.
+                # iclamp.amp=cell.alpha * np.dot(whitenoise[t],cell.encoder) + cell.bias #access alpha/bias/encoder?
+                iclamp.amp=whitenoise[x]
+                neuron.run(neuron.h.t+nrn_duration(runtime_per_eval)) #advance by runtime_per_eval ms
+                #if the number of spikes in apcount is greater than last timestep, append spike to list
+                spikes[x]=int(apcount.n - np.sum(spikes))
+            t=np.arange(int(t_run/dt))*dt
+            h=np.exp(-t/tau_filter) #smooth spikes with exponential synaptic filter
+            h=h/np.sum(h*dt)
+            rates=np.array(np.convolve(spikes,h,mode='full')[:len(spikes)])
+            # print 'spikes',spikes
+            # print 'rates',rates
+            A.append(rates)
+
+        #calculate decoders from activities
+        A_T=np.matrix(A)
+        A=A_T.T
+        X=np.matrix(whitenoise)
+        upsilon=A_T*function(X)/len(X)
+        gamma=A_T*A/len(X)
+        d=np.linalg.pinv(gamma)*upsilon
+        # print 'decoders=%s' %d
+
+        return d
+
+def make_df(sim,P,A_probe,B_LIF_probe,B_NRN_probe,C_LIF_probe,C_NRN_probe):
     import pandas as pd
     import numpy as np
     import ipdb
@@ -14,7 +108,7 @@ def make_df(sim,P,stim_probe,B_LIF_probe,B_NRN_probe,C_LIF_probe,C_NRN_probe):
     i=0
     for t in timesteps:
         rt=t*P['dt_sample']
-        a=sim.data[stim_probe][t][0]
+        a=sim.data[A_probe][t][0]
         b_lif=sim.data[B_LIF_probe][t][0]
         b_NRN=sim.data[B_NRN_probe][t][0]
         c_lif=sim.data[C_LIF_probe][t][0]
@@ -47,38 +141,39 @@ def run(P):
         B_NRN = nengo.Ensemble(P['n_neurons_B_NRN'], dimensions=2, neuron_type=Bahr2(), max_rates=Uniform(60, 80))
         C_LIF = nengo.Ensemble(P['n_neurons_C_LIF'], dimensions=1)
         C_NRN = nengo.Ensemble(P['n_neurons_C_NRN'], dimensions=1)
-        # C_NRN = nengo.Ensemble(P['n_neurons_C_NRN'], dimensions=1, neuron_type=Bahr2(), max_rates=Uniform(60, 80))
 
         nengo.Connection(stim, A[0])
         nengo.Connection(ramp, A[1])
         solver_A_into_B = nengo.solvers.LstsqL2(True)
-        # solver_B_into_C = nengo.solvers.LstsqL2(True)
+        # solver_A_into_B = whitenoise_ndn_decoders(B_NRN)
         B_LIF_into_C_LIF = nengo.Connection(B_LIF[0], C_LIF)
         B_NRN_into_C_NRN = nengo.Connection(B_NRN[0], C_NRN)
-        # B_NRN_into_C_NRN = nengo.Connection(B_NRN[0], C_NRN, solver=solver_B_into_C, synapse=ExpSyn(P['tau']))
 
-        if P['integrator']==True:
-            A_into_B_LIF=nengo.Connection(A, B_LIF,
-                                            transform=P['tau_recurrent'], synapse=P['tau_recurrent'])
-            A_into_B_NRN=nengo.Connection(A, B_NRN, solver=solver_A_into_B, 
-                                            transform=P['tau_recurrent'], synapse=ExpSyn(P['tau_recurrent']))
-            # A_into_B_LIF.transform=P['tau_recurrent']
-            # A_into_B_LIF.synapse=nengo.Lowpass(tau=P['tau_recurrent'])
-            # A_into_B_NRN.transform=P['tau_recurrent']
-            # A_into_B_NRN.synapse=nengo.Lowpass(tau=P['tau_recurrent'])
+        if P['type']=='communication':
+            nengo.Connection(A, B_LIF, synapse=P['tau'])
+            nengo.Connection(A, B_NRN, solver=solver_A_into_B, synapse=ExpSyn(P['tau']))
+        elif P['type']=='integrator':
+            nengo.Connection(A, B_LIF,transform=P['tau_recurrent'], synapse=P['tau_recurrent'])
+            nengo.Connection(A, B_NRN, solver=solver_A_into_B, 
+                                transform=P['tau_recurrent'], synapse=ExpSyn(P['tau_recurrent']))
             solver_recurrent_B = nengo.solvers.LstsqL2(True)
             nengo.Connection(B_LIF, B_LIF, synapse=P['tau_recurrent'])
             nengo.Connection(B_NRN, B_NRN, solver=solver_recurrent_B, synapse=ExpSyn(P['tau_recurrent']))
-        else:
-            A_into_B_LIF=nengo.Connection(A, B_LIF, synapse=P['tau'])
-            A_into_B_NRN=nengo.Connection(A, B_NRN, solver=solver_A_into_B, synapse=ExpSyn(P['tau']))
+        elif P['type']=='chain':
+            B0_LIF = nengo.Ensemble(P['n_neurons_B_LIF'], dimensions=2)
+            B0_NRN = nengo.Ensemble(P['n_neurons_B_NRN'], dimensions=2, neuron_type=Bahr2(), max_rates=Uniform(60, 80))
+            nengo.Connection(A, B0_LIF, synapse=P['tau'])
+            nengo.Connection(A, B0_NRN, solver=solver_A_into_B, synapse=ExpSyn(P['tau']))            
+            solver_B0_into_B = nengo.solvers.LstsqL2(True)
+            nengo.Connection(B0_LIF, B_LIF, synapse=P['tau_recurrent'])
+            nengo.Connection(B0_NRN, B_NRN, solver=solver_B0_into_B, synapse=ExpSyn(P['tau_recurrent']))
 
         if P['noise_B']>0.0:
             noise = nengo.Node(output=np.random.normal(P['drug_effect_stim'][P['drug']],P['noise_B']))
             nengo.Connection(noise,B_LIF.neurons,synapse=P['tau'],transform=np.ones((P['num_B_neurons'],1))*P['tau'])
             nengo.Connection(noise,B_NRN.neurons,synapse=P['tau'],transform=np.ones((P['num_B_neurons'],1))*P['tau'])
 
-        a_probe = nengo.Probe(A[0], synapse=0.01, sample_every=P['dt_sample'])
+        A_probe = nengo.Probe(A[0], synapse=0.01, sample_every=P['dt_sample'])
         B_LIF_probe = nengo.Probe(B_LIF[0], synapse=0.01, sample_every=P['dt_sample'])
         B_NRN_probe = nengo.Probe(B_NRN[0], synapse=0.01, sample_every=P['dt_sample'])
         C_LIF_probe = nengo.Probe(C_LIF, synapse=0.01, sample_every=P['dt_sample'])
@@ -86,9 +181,10 @@ def run(P):
 
     print 'Running drug %s trial %s...' %(P['drug'],P['trial']+1)
     with nengo.Simulator(model,dt=P['dt']) as sim:
-        B_NRN=reset_channels(P,B_NRN)
+        if P['solver_type']=='whitenoise': sim=calculate_NRN_decoders(P,model,sim)
+        # B_NRN=reset_channels(P,B_NRN)
         sim.run(P['t_stim']+P['t_delay'])
-        df=make_df(sim,P,a_probe,B_LIF_probe,B_NRN_probe,C_LIF_probe,C_NRN_probe)
+        df=make_df(sim,P,A_probe,B_LIF_probe,B_NRN_probe,C_LIF_probe,C_NRN_probe)
     return df
 
 def init_directory():
@@ -149,7 +245,7 @@ def main():
             my_P['drug']=drug
             my_P['trial']=trial
             exp_params.append(my_P)
-    # df=run(exp_params[0])
+    df=run(exp_params[0])
 
     init_directory()
     pool = Pool2(nodes=P['threads'])
--- wm_5_gui.py
+++ wm_5_gui.py
@@ -19,45 +19,45 @@ import ipdb
 #simulation parameters
 seed=3 #for the simulator build process, sets tuning curves equal to control before drug application
 trial=0
-dt=0.001 #timestep
+dt=0.001 #Timestep
 dt_sample=0.05 #probe sample_every
-t_cue=1.0 #duration of cue presentation
-t_delay=8.0 #duration of delay period between cue and decision
-timesteps=np.arange(0,int((t_cue+t_delay)/dt_sample))
+t_Cue=1.0 #duration of Cue presentation
+t_delay=8.0 #duration of delay period between Cue and Decision
+Timesteps=np.arange(0,int((t_Cue+t_delay)/dt_sample))
 
 drug='control'
-decision_type='BG' #which decision procedure to use: 'choice' for noisy choice, 'BG' basal ganglia
+Decision_type='choice' #which Decision procedure to use: 'choice' for noisy choice, 'BG' basal ganglia
 drug_type='addition' #how to simulate the drugs: 'addition','multiply',alpha','NEURON',
 
-drug_effect_cue={'control':0.0,'PHE':-0.3,'GFC':0.5,'no_ramp':0.0} #mean of injected cueulus onto wm.neurons
-drug_effect_multiply={'control':0.0,'PHE':-0.025,'GFC':0.025} #mean of injected cueulus onto wm.neurons
-drug_effect_gain={'control':[1.0,1,0],'PHE':[0.99,1.02],'GFC':[1.05,0.95]} #multiplier for alpha/bias in wm
+drug_effect_Cue={'control':0.0,'PHE':-0.3,'GFC':0.5,'no_Time':0.0} #mean of injected Cueulus onto WM.neurons
+drug_effect_multiply={'control':0.0,'PHE':-0.025,'GFC':0.025} #mean of injected Cueulus onto WM.neurons
+drug_effect_gain={'control':[1.0,1,0],'PHE':[0.99,1.02],'GFC':[1.05,0.95]} #multiplier for alpha/bias in WM
 drug_effect_channel={'control':200.0,'PHE':230,'GFC':160} #multiplier for channel conductances in NEURON cells
 
-k_neuron_inputs=1.0
+k_neuron_Inputs=1.0
 k_neuron_recur=1.0
-delta_rate=0.00 #increase the maximum firing rate of wm neurons for NEURON
+delta_rate=0.00 #increase the maximum firing rate of WM neurons for NEURON
 
 enc_min_cutoff=0.3 #minimum cutoff for "weak" encoders in preferred directions
 enc_max_cutoff=0.6 #maximum cutoff for "weak" encoders in preferred directions
 sigma_smoothing=0.005 #gaussian smoothing applied to spike data to calculate firing rate
 frac=0.05 #fraction of neurons in WM to add to dataframe and plot
 
-neurons_inputs=200 #neurons for the inputs ensemble
-neurons_wm=100 #neurons for workimg memory ensemble
-neurons_decide=100 #neurons for decision or basal ganglia
-ramp_scale=0.42 #how fast does the 'time' dimension accumulate in WM neurons, default=0.42
-cue_scale=1.0 #how strong is the cueulus from the visual system
-tau_cue=None #synaptic time constant of cueuli to populations
-tau=0.01 #synaptic time constant between ensembles
-tau_wm=0.1 #synapse on recurrent connection in wm
-noise_wm=0.005 #standard deviation of full-spectrum white noise injected into wm.neurons
-noise_decision=0.3 #for addition, std of added gaussian noise; 
-wm_decay=1.0 #recurrent transform in wm ensemble: set <1.0 for decay
+neurons_Inputs=200 #neurons for the Inputs ensemble
+neurons_WM=100 #neurons for workimg memory ensemble
+neurons_decide=100 #neurons for Decision or basal ganglia
+Time_scale=0.42 #how fast does the 'Time' dimension accumulate in WM neurons, default=0.42
+Cue_scale=1.0 #how strong is the Cueulus from the visual system
+tau_Cue=None #synaptic Time constant of Cueuli to populations
+tau=0.01 #synaptic Time constant between ensembles
+tau_WM=0.1 #synapse on recurrent connection in WM
+Noise_WM=0.005 #standard deviation of full-spectrum white Noise injected into WM.neurons
+Noise_Decision=0.3 #for addition, std of added gaussian Noise; 
+WM_decay=1.0 #recurrent transform in WM ensemble: set <1.0 for decay
 
-misperceive=0.1 #chance of failing to perceive the cue, causing no info to go into WM
-perceived=np.ones(1) #list of correctly percieved (not necessarily remembered) cues
-cues=2*np.random.randint(2,size=1)-1 #whether the cues is on the left or right
+misperceive=0.1 #chance of failing to perceive the Cue, causing no info to go into WM
+perceived=np.ones(1) #list of correctly percieved (not necessarily remembered) Cues
+Cues=2*np.random.randint(2,size=1)-1 #whether the Cues is on the left or right
 for n in range(len(perceived)): 
 	if np.random.rand()<misperceive: perceived[n]=0
 plot_context='poster' #seaborn plot context
@@ -73,131 +73,131 @@ class MySolver(nengo.solvers.Solver):
 		def __call__(self,A,Y,rng=None,E=None): #the function that gets called by the builder
 			return self.my_weights.T, dict()
 
-def cue_function(t):
-	if t < t_cue and perceived[trial]!=0:
-		return cue_scale * cues[trial]
+def Cue_function(t):
+	if t < t_Cue and perceived[trial]!=0:
+		return Cue_scale * Cues[trial]
 	else: return 0
 
-def ramp_function(t):
-	if drug=='no_ramp': return 0
+def Time_function(t):
+	if drug=='no_Time': return 0
 	elif drug_type == 'NEURON' and drug_type == 'NEURON':
-		return ramp_scale * k_neuron_inputs
-	elif t > t_cue:
-		return ramp_scale
+		return Time_scale * k_neuron_Inputs
+	elif t > t_Cue:
+		return Time_scale
 	else: return 0
 
-def noise_bias_function(t):
+def Noise_bias_function(t):
 	if drug_type=='addition':
-		return np.random.normal(drug_effect_cue[drug],noise_wm)
+		return np.random.normal(drug_effect_Cue[drug],Noise_WM)
 	else:
-		return np.random.normal(0.0,noise_wm)
+		return np.random.normal(0.0,Noise_WM)
 
-def noise_decision_function(t):
-	if decision_type == 'choice':
-		return np.random.normal(0.0,noise_decision)
-	elif decision_type == 'BG':
-		return np.random.normal(0.0,noise_decision,size=2)
+def Noise_Decision_function(t):
+	if Decision_type == 'choice':
+		return np.random.normal(0.0,Noise_Decision)
+	elif Decision_type == 'BG':
+		return np.random.normal(0.0,Noise_Decision,size=2)
 
-def inputs_function(x):
+def Inputs_function(x):
 	if drug_type == 'NEURON':
-		return x * tau_wm * k_neuron_inputs
+		return x * tau_WM * k_neuron_Inputs
 	else:
-		return x * tau_wm
+		return x * tau_WM
 
-def wm_recurrent_function(x):
+def WM_recurrent_function(x):
 	if drug_type == 'multiply':
-		return x * (wm_decay + drug_effect_multiply[drug])
+		return x * (WM_decay + drug_effect_multiply[drug])
 	elif drug_type=='NEURON':
-		return x * wm_decay * k_neuron_recur
+		return x * WM_decay * k_neuron_recur
 	else:
-		return x * wm_decay
+		return x * WM_decay
 
-def decision_function(x):
-	output=0.0
-	if decision_type=='choice':
+def Decision_function(x):
+	Output=0.0
+	if Decision_type=='choice':
 		value=x[0]+x[1]
-		if value > 0.0: output = 1.0
-		elif value < 0.0: output = -1.0
-	elif decision_type=='BG':
-		if x[0] > x[1]: output = 1.0
-		elif x[0] < x[1]: output = -1.0
-	return output 
+		if value > 0.0: Output = 1.0
+		elif value < 0.0: Output = -1.0
+	elif Decision_type=='BG':
+		if x[0] > x[1]: Output = 1.0
+		elif x[0] < x[1]: Output = -1.0
+	return Output 
 
 def BG_rescale(x): #rescales -1 to 1 into 0.3 to 1
 	pos_x = 2 * x + 1
 	rescaled = 0.3 + 0.7 * pos_x, 0.3 + 0.7 * (1 - pos_x)
 	return rescaled
 
-def reset_alpha_bias(model,sim,wm_recurrent,wm_choice,wm_BG,drug):
+def reset_alpha_bias(model,sim,WM_recurrent,WM_choice,WM_BG,drug):
 	#set gains and biases as a constant multiple of the old values
-	wm.gain = sim.data[wm].gain * drug_effect_gain[drug][0]
-	wm.bias = sim.data[wm].bias * drug_effect_gain[drug][1]
-	#set the solver of each of the connections coming out of wm using the custom MySolver class
+	WM.gain = sim.data[WM].gain * drug_effect_gain[drug][0]
+	WM.bias = sim.data[WM].bias * drug_effect_gain[drug][1]
+	#set the solver of each of the connections coming out of WM using the custom MySolver class
 	#with input equal to the old decoders. We use the old decoders because we don't want the builder
 	#to optimize the decoders to the new alpha/bias values, otherwise it would "adapt" to the drug
-	wm_recurrent.solver = MySolver(sim.model.params[wm_recurrent].weights)
-	if wm_choice is not None:
-		wm_choice.solver = MySolver(sim.model.params[wm_choice].weights)
-	if wm_BG is not None:
-		wm_BG.solver = MySolver(sim.model.params[wm_BG].weights[0]) 
+	WM_recurrent.solver = MySolver(sim.model.params[WM_recurrent].weights)
+	if WM_choice is not None:
+		WM_choice.solver = MySolver(sim.model.params[WM_choice].weights)
+	if WM_BG is not None:
+		WM_BG.solver = MySolver(sim.model.params[WM_BG].weights[0]) 
 	#rebuild the network to affect the gain/bias change	
 	sim=nengo.Simulator(model,dt=dt)
 	return sim
 
 def reset_channels(drug):
 	#strongly enhance the I_h current, by opening HCN channels, to create shunting under control
-	for cell in nengo_detailed_neurons.builder.ens_to_cells[wm]:
+	for cell in nengo_detailed_neurons.builder.ens_to_cells[WM]:
 	    cell.neuron.tuft.gbar_ih *= drug_effect_channel[drug]
 	    cell.neuron.apical.gbar_ih *= drug_effect_channel[drug]
 	    cell.neuron.recalculate_channel_densities()
 
 '''dataframe initialization ###############################################'''
 
-def primary_dataframe(sim,drug,trial,probe_wm,probe_output):
-	columns=('time','drug','wm','output','correct','trial') 
-	df_primary = pd.DataFrame(columns=columns, index=np.arange(0,len(timesteps)))
+def primary_dataframe(sim,drug,trial,probe_WM,probe_Output):
+	columns=('Time','drug','WM','Output','correct','trial') 
+	df_primary = pd.DataFrame(columns=columns, index=np.arange(0,len(Timesteps)))
 	i=0
-	for t in timesteps:
-		wm_val=np.abs(sim.data[probe_wm][t][0])
-		output_val=sim.data[probe_output][t][0]
-		correct=get_correct(cues[trial],output_val)
+	for t in Timesteps:
+		WM_val=np.abs(sim.data[probe_WM][t][0])
+		Output_val=sim.data[probe_Output][t][0]
+		correct=get_correct(Cues[trial],Output_val)
 		rt=t*dt_sample
-		df_primary.loc[i]=[rt,drug,wm_val,output_val,correct,trial]
+		df_primary.loc[i]=[rt,drug,WM_val,Output_val,correct,trial]
 		i+=1
 	return df_primary
 
-def firing_dataframe(sim,drug,trial,sim_wm,probe_spikes):
-	columns=('time','drug','neuron-trial','tuning','firing_rate')
-	df_firing = pd.DataFrame(columns=columns, index=np.arange(0,len(timesteps)*int(neurons_wm*frac)))
+def firing_dataframe(sim,drug,trial,sim_WM,probe_spikes):
+	columns=('Time','drug','neuron-trial','tuning','firing_rate')
+	df_firing = pd.DataFrame(columns=columns, index=np.arange(0,len(Timesteps)*int(neurons_WM*frac)))
 	t_width = 0.2
 	t_h = np.arange(t_width / dt) * dt - t_width / 2.0
 	h = np.exp(-t_h ** 2 / (2 * sigma_smoothing ** 2))
 	h = h / np.linalg.norm(h, 1)
 	j=0
-	for nrn in range(int(neurons_wm*frac)):
-		enc = sim_wm.encoders[nrn]
-		tuning = get_tuning(cues[trial],enc)
+	for nrn in range(int(neurons_WM*frac)):
+		enc = sim_WM.encoders[nrn]
+		tuning = get_tuning(Cues[trial],enc)
 		spikes = sim.data[probe_spikes][:,nrn]		
 		firing_rate = np.convolve(spikes,h,mode='same')
-		for t in timesteps:
+		for t in Timesteps:
 			rt=t*dt_sample
-			df_firing.loc[j]=[rt,drug,nrn+trial*neurons_wm,tuning,firing_rate[t]]
+			df_firing.loc[j]=[rt,drug,nrn+trial*neurons_WM,tuning,firing_rate[t]]
 			j+=1
 		# print 'appending dataframe for neuron %s' %f
 	return df_firing
 
-def get_correct(cue,output_val):
-	if (cue > 0.0 and output_val > 0.0) or (cue < 0.0 and output_val < 0.0): correct=1
+def get_correct(Cue,Output_val):
+	if (Cue > 0.0 and Output_val > 0.0) or (Cue < 0.0 and Output_val < 0.0): correct=1
 	else: correct=0
 	return correct
 			
-def get_tuning(cue,enc):
-	if (cue > 0.0 and 0.0 < enc[0] < enc_min_cutoff) or \
-		(cue < 0.0 and 0.0 > enc[0] > -1.0*enc_min_cutoff): tuning='superweak'
-	if (cue > 0.0 and enc_min_cutoff < enc[0] < enc_max_cutoff) or \
-		(cue < 0.0 and -1.0*enc_max_cutoff < enc[0] < -1.0*enc_min_cutoff): tuning='weak'
-	elif (cue > 0.0 and enc[0] > enc_max_cutoff) or \
-		(cue < 0.0 and enc[0] < -1.0*enc_max_cutoff): tuning='strong'
+def get_tuning(Cue,enc):
+	if (Cue > 0.0 and 0.0 < enc[0] < enc_min_cutoff) or \
+		(Cue < 0.0 and 0.0 > enc[0] > -1.0*enc_min_cutoff): tuning='superweak'
+	if (Cue > 0.0 and enc_min_cutoff < enc[0] < enc_max_cutoff) or \
+		(Cue < 0.0 and -1.0*enc_max_cutoff < enc[0] < -1.0*enc_min_cutoff): tuning='weak'
+	elif (Cue > 0.0 and enc[0] > enc_max_cutoff) or \
+		(Cue < 0.0 and enc[0] < -1.0*enc_max_cutoff): tuning='strong'
 	else: tuning='nonpreferred'
 	return tuning
 
@@ -207,59 +207,59 @@ def get_tuning(cue,enc):
 with nengo.Network(seed=seed+trial) as model:
     #Ensembles
     #Inputs
-    cue = nengo.Node(output=cue_function)
-    ramp = nengo.Node(output=ramp_function)
-    inputs = nengo.Ensemble(neurons_inputs,2)
-    noise_wm = nengo.Node(output=noise_bias_function)
-    noise_decision = nengo.Node(output=noise_decision_function)
+    Cue = nengo.Node(output=Cue_function)
+    Time = nengo.Node(output=Time_function)
+    Inputs = nengo.Ensemble(neurons_Inputs,2)
+    Noise_WM = nengo.Node(output=Noise_bias_function)
+    Noise_Decision = nengo.Node(output=Noise_Decision_function)
     #Working Memory
     if drug_type == 'NEURON':
-    	wm = nengo.Ensemble(neurons_wm,2,neuron_type=Bahr2(),max_rates=Uniform(200+delta_rate,400+delta_rate))
+    	WM = nengo.Ensemble(neurons_WM,2,neuron_type=Bahr2(),max_rates=Uniform(200+delta_rate,400+delta_rate))
     else:
-    	wm = nengo.Ensemble(neurons_wm,2)
+    	WM = nengo.Ensemble(neurons_WM,2)
     #Decision
-    if decision_type=='choice':
-    	decision = nengo.Ensemble(neurons_decide,2)
-    elif decision_type=='BG':
-    	utilities = nengo.networks.EnsembleArray(neurons_inputs,n_ensembles=2)
+    if Decision_type=='choice':
+    	Decision = nengo.Ensemble(neurons_decide,2)
+    elif Decision_type=='BG':
+    	utilities = nengo.networks.EnsembleArray(neurons_Inputs,n_ensembles=2)
     	BG = nengo.networks.BasalGanglia(2,neurons_decide)
-    	decision = nengo.networks.EnsembleArray(neurons_decide,n_ensembles=2,
+    	Decision = nengo.networks.EnsembleArray(neurons_decide,n_ensembles=2,
     				intercepts=Uniform(0.2,1),encoders=Uniform(1,1))
     	temp = nengo.Ensemble(neurons_decide,2)
     	bias = nengo.Node([1]*2)
     #Output
-    output = nengo.Ensemble(neurons_decide,1)
+    Output = nengo.Ensemble(neurons_decide,1)
     
     #Connections
-    nengo.Connection(cue,inputs[0],synapse=tau_cue)
-    nengo.Connection(ramp,inputs[1],synapse=tau_cue)
+    nengo.Connection(Cue,Inputs[0],synapse=tau_Cue)
+    nengo.Connection(Time,Inputs[1],synapse=tau_Cue)
     if drug_type == 'NEURON':
-    	solver_cue = nengo.solvers.LstsqL2(True)
-    	solver_wm = nengo.solvers.LstsqL2(True)
-    	nengo.Connection(inputs,wm,synapse=ExpSyn(tau_wm),function=inputs_function,solver=solver_cue)
-    	wm_recurrent=nengo.Connection(wm,wm,synapse=ExpSyn(tau_wm),function=wm_recurrent_function,solver=solver_wm)	
-    	nengo.Connection(noise_wm,wm.neurons,synapse=tau_wm,transform=np.ones((neurons_wm,1))*tau_wm)							
+    	solver_Cue = nengo.solvers.LstsqL2(True)
+    	solver_WM = nengo.solvers.LstsqL2(True)
+    	nengo.Connection(Inputs,WM,synapse=ExpSyn(tau_WM),function=Inputs_function,solver=solver_Cue)
+    	WM_recurrent=nengo.Connection(WM,WM,synapse=ExpSyn(tau_WM),function=WM_recurrent_function,solver=solver_WM)	
+    	nengo.Connection(Noise_WM,WM.neurons,synapse=tau_WM,transform=np.ones((neurons_WM,1))*tau_WM)							
     else:
-    	nengo.Connection(inputs,wm,synapse=tau_wm,function=inputs_function)
-    	wm_recurrent=nengo.Connection(wm,wm,synapse=tau_wm,function=wm_recurrent_function)
-    	nengo.Connection(noise_wm,wm.neurons,synapse=tau_wm,transform=np.ones((neurons_wm,1))*tau_wm)
-    wm_choice,wm_BG=None,None
-    if decision_type=='choice':	
-    	wm_choice=nengo.Connection(wm[0],decision[0],synapse=tau) #no ramp information passed
-    	nengo.Connection(noise_decision,decision[1],synapse=None)
-    	nengo.Connection(decision,output,function=decision_function)
-    elif decision_type=='BG':
-    	# wm_BG=nengo.Connection(wm[0],utilities.input,synapse=tau,transform=[[1],[-1]])
-    	wm_BG=nengo.Connection(wm[0],utilities.input,synapse=tau,function=BG_rescale)
-    	nengo.Connection(utilities.output,BG.input,synapse=None)
-    	nengo.Connection(BG.output,decision.input,synapse=tau)
-    	nengo.Connection(noise_decision,BG.input,synapse=None) #added external noise?
-    	nengo.Connection(bias,decision.input,synapse=tau)
-    	nengo.Connection(decision.input,decision.output,transform=(np.eye(2)-1),synapse=tau/2.0)
-    	nengo.Connection(decision.output,temp)
-    	nengo.Connection(temp,output,function=decision_function)
+    	nengo.Connection(Inputs,WM,synapse=tau_WM,function=Inputs_function)
+    	WM_recurrent=nengo.Connection(WM,WM,synapse=tau_WM,function=WM_recurrent_function)
+    	nengo.Connection(Noise_WM,WM.neurons,synapse=tau_WM,transform=np.ones((neurons_WM,1))*tau_WM)
+    WM_choice,WM_BG=None,None
+    if Decision_type=='choice':	
+    	WM_choice=nengo.Connection(WM[0],Decision[0],synapse=tau) #no Time information passed
+    	nengo.Connection(Noise_Decision,Decision[1],synapse=None)
+    	nengo.Connection(Decision,Output,function=Decision_function)
+    elif Decision_type=='BG':
+    	# WM_BG=nengo.Connection(WM[0],utilities.input,synapse=tau,transform=[[1],[-1]])
+    	WM_BG=nengo.Connection(WM[0],utilities.input,synapse=tau,function=BG_rescale)
+    	nengo.Connection(utilities.Output,BG.input,synapse=None)
+    	nengo.Connection(BG.Output,Decision.input,synapse=tau)
+    	nengo.Connection(Noise_Decision,BG.input,synapse=None) #added external Noise?
+    	nengo.Connection(bias,Decision.input,synapse=tau)
+    	nengo.Connection(Decision.input,Decision.Output,transform=(np.eye(2)-1),synapse=tau/2.0)
+    	nengo.Connection(Decision.Output,temp)
+    	nengo.Connection(temp,Output,function=Decision_function)
 
 # with nengo.Simulator(model,dt=dt) as sim:
-#     if drug_type == 'alpha': sim=reset_alpha_bias(model,sim,wm_recurrent,wm_choice,wm_BG,drug)
+#     if drug_type == 'alpha': sim=reset_alpha_bias(model,sim,WM_recurrent,WM_choice,WM_BG,drug)
 #     if drug_type == 'NEURON': reset_channels(drug)
-#     sim.run(t_cue+t_delay)
+#     sim.run(t_Cue+t_delay)
